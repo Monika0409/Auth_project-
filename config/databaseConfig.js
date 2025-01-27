d7f6e19a3173375a57fc94fe3ef95f9d6423bdeb
Monika0409 <118674595+Monika0409@users.noreply.github.com>
2025-01-28 00:14:45 +0530

    databaseConfig.js

--- config/databaseConfig.js
+++ config/databaseConfig.js
@@ -1,15 +1,14 @@
-const mongoose = require('mongoose');        // require mongoose
+const mongoose = require('mongoose');       
 
-const MONGODB_URL = process.env.MONGODB_URL || "mongodb://localhost:27017/my_database";  // mongodb url
+const MONGODB_URL = process.env.MONGODB_URL || "mongodb://localhost:27017/my_database";  
 
 const databaseconnect = () => {
     mongoose
         .connect(MONGODB_URL)
         .then((conn) => console.log(`Connected to DB: ${conn.connection.host}`))
         .catch((err) => console.log(err.message));
-}                      // for database connect
+}                      
 
-module.exports = databaseconnect;   // for comsuming anywhere
+module.exports = databaseconnect;   
 
 
-// alg alg environment me alg alg configaration use karne ke liye || third party setup ke liye hum env ka use karte hai
