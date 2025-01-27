193431c36805e6dc995fafa7513941ddab9fec62
Monika0409 <118674595+Monika0409@users.noreply.github.com>
2025-01-28 00:10:57 +0530

    authRoute.js

--- router/authRoute.js
+++ router/authRoute.js
@@ -1,11 +1,11 @@
-const express = require('express');     // require expree
+const express = require('express');     
 const { signup, signin, getUser, logout } = require('../controller/authController');
 const jwtAuth = require('../middleware/jwtAuth')
-const authRouter = express.Router();         // router ch instance
+const authRouter = express.Router();         
 
-authRouter.post('/signup', signup);    // it take path and controller       create entity-post
-authRouter.post('/signin', signin);    // for signin
-authRouter.get('/user', jwtAuth ,getUser)  // for get the user info
+authRouter.post('/signup', signup);    
+authRouter.post('/signin', signin);    
+authRouter.get('/user', jwtAuth ,getUser)  
 authRouter.get('/logout', jwtAuth, logout)
 
-module.exports = authRouter;       // for use anywhere
+module.exports = authRouter;       
