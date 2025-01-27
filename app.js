4c8c01c725a79acc83592f0e725dfbab07de40ff
Monika0409 <118674595+Monika0409@users.noreply.github.com>
2025-01-28 00:09:44 +0530

    app.js

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
-const express = require('express');           // require express
-const app = express();                        // import express
-const authRouter = require('./router/authRoute');  // requirauthRouter 
+const express = require('express');           
+const app = express();                        
+const authRouter = require('./router/authRoute');  
 const databaseconnect = require('./config/databaseConfig');
-const cookieParser = require('cookie-parser');     // require cookieparser
-const cors = require('cors');      // require cors
+const cookieParser = require('cookie-parser');     
+const cors = require('cors');      
 
-databaseconnect()   // database connect
+databaseconnect()   
 
-app.use(express.json());         // for giving json type data
-app.use(cookieParser());         // pass the cookie parser
+app.use(express.json());         
+app.use(cookieParser());         
 app.use(cors({
     origin : [process.env.CLIENT_URL],
     credentials : true
-}))                              // define cors
+}))                              
  
-app.use('/api/auth/', authRouter);     // take path and router 
+app.use('/api/auth/', authRouter);     
 
 app.use('/', (req, res) => {
     res.status(200).json({data: 'JWTauth server'});
 });
 
-module.exports = app;     // for use anywhere
+module.exports = app;     
 
 
